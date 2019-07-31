500168619c11e8528e1b33084d4d83a0a4df65f1
gergokutu <gergo.kutu@gmail.com>
2019-07-31 15:16:04 +0200

    CRUD routes def » team/route

--- team/router.js
+++ team/router.js
@@ -21,15 +21,66 @@ router.get(
 router.post(
   '/team',
   (request, response, next) => {
-    Team.create(request.body)
+    Team
+    // findOne » have to find 1 where the name is...
+    // the request.body.name
+    // then » if it exists (true) » do not do anything...
+    // just send a status code and answer
+      .findOne({ where: { name: request.body.name }})
       .then(team => {
-        response.send(team)
+        if (team) {
+          // proper order to send both http status code and a message
+          response.status(403).send("Name already used.",)
+        } else {
+          // returning Team here to not to use catch 2x
+          return Team
+            .create(request.body)
+            .then(team => response.send(team))
+            // next catch is not necessary
+            // .catch(error => next(error))
+        }
+      })
+      .catch(next)
+  }
+)
+
+router.get(
+  '/team/:id',
+  (request, response, next) => {
+    Team.findByPk(request.params.id)
+      .then(team => response.send(team))
+      .catch(error => next(error))
+  }
+)
+
+router.put(
+  '/team/:id',
+  (request, response, next) => {
+    Team
+      .findByPk(request.params.id)
+      .then(team => {
+        if (team) {
+          return team.update(request.body)
+            .then(team => response.send(team))
+        }
+        return response.status(404).end()
       })
       .catch(error => next(error))
   }
 )
       
-
-
+router.delete(
+  '/team/:id',
+  (request, response, next) => {
+    Team
+      .destroy({
+        where: {
+          id: request.params.id
+        }
+      })
+      .then(number => response.send({ number }))
+      .catch(next)
+  }
+)
 
 module.exports = router
