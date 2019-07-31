cc6613853334ce64a100e946c2b3c9604e38f0b6
gergokutu <gergo.kutu@gmail.com>
2019-07-31 12:19:55 +0200

    Sequelize setup, db.js » postgres connected, team/model.js » football_teams table created

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// importing express
 const express = require('express')
+// for testing purpose
+// const db = require('./db')
+const Team = require('./team/model')
+
 // 'instantiating' express
 const app = express()
 // configuring the port (we might get one if we are putting our app online)
 const port = process.env.PORT || 4000
+
 
 // Pass the port to app.listen to start the server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
